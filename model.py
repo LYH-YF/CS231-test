ef1c99c9a500e496b730bc761d3e678a2a5136d0
LYH-YF <yifan2250@gmail.com>
2020-10-20 18:36:05 +0800

    add knn model

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+from torch import tensor
 class NearestNeighbor(object):
     def __init__(self,X,y,p,k) -> None:
         self.X=X
@@ -20,7 +22,39 @@ class NearestNeighbor(object):
             y_test[i]=np.argmax(np.bincount(labels))
             
         return y_test
+class NearestNeighbor_torch(object):
+    def __init__(self,p,k,cuda_use):
+        self.p=p
+        self.k=k
+        self.cuda_use=cuda_use
+    def train(self,X,y):
+        self.X=X
+        self.y=y
+        if self.cuda_use:
+            self.X=self.X.cuda()
+            self.y=self.y.cuda()
+    def predict(self,X):
+        if self.cuda_use:
+            X=X.cuda()
+            y=y.cuda()
+        num_test=X.shape[0]
+        y_test=torch.zeros(num_test,dtype=self.y.dtype)
+        for i in range(num_test):
+            print("\rpredict [{}/{}]".format(i+1,num_test),end="")
+            if self.p=="L2":
+                distence=torch.sqrt(torch.sum(torch.square(self.X-X[i,:]),dim=1))
+            else:
+                distence=torch.sum(torch.abs(self.X-X[i,:]),dim=1)
+            k_min_d,k_min_idx=distence.topk(self.k,largest=False)
+            k_min_idx=torch.tensor(k_min_idx)
+            labels=self.y[k_min_idx]
+            y_test[i]=torch.argmax(torch.bincount(labels))
+        return y_test
 if __name__ == "__main__":
-    a=np.array([1,25,3,6,8,12,7,0,-1,9])
-    x=a.argsort()[::-1][0:3]
-    print(x)
+    a=tensor([1,25,3,6,8,12,7,0,-1,9])
+    b=np.array([1,25,3,6,8,12,7,0,-1,9])
+    x,y=a.topk(3,largest=False)
+    print(x,y)
+    print(a.argsort())
+    print(b.argsort())
+    
--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 import numpy as np
 from dataprocess import CIFAR10_DataLoading
-from model import NearestNeighbor
+from model import NearestNeighbor,NearestNeighbor_torch
+import torch
 def KNNrunner():
     train_data,train_label,test_data,test_label=CIFAR10_DataLoading()
     train_label=np.array(train_label)
@@ -21,5 +22,22 @@ def KNNrunner():
     result=y_pred==test_label
     acc_rate=sum(result==True)/len(result)
     print("acc:",acc_rate)
+def KNNrunner_torch():
+    cuda_use= True if torch.cuda.is_available() else False
+    
+    train_data,train_label,test_data,test_label=CIFAR10_DataLoading()
+    train_data=torch.tensor(train_data)
+    train_label=torch.tensor(train_label)
+    test_data=torch.tensor(test_data)
+    test_label=torch.tensor(test_label)
+    
+    knn_model=NearestNeighbor_torch("L1",100,cuda_use)
+    knn_model.train(train_data,train_label)
+    y_pred=knn_model.predict(test_data)
+    
+    result=y_pred==test_label
+    acc_rate=sum(result==True).float()/len(result)
+    print("acc:",acc_rate)
 if __name__ == "__main__":
-    KNNrunner()
+    #KNNrunner()
+    KNNrunner_torch()
